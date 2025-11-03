19c43401033cf7f627dabe6ba026c9139b5e686e
Martin Danmo <martindanmo81@gmail.com>
2025-11-03 07:56:21 +0100

    Pusselspel

---
+++ src/PuzzleDialog.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PuzzleDialog extends JFrame implements ActionListener
+{
+    private JPanel gamePanel;
+    private JPanel progressPanel;
+    private JPanel buttonPanel;
+    private JButton[] buttons;
+    private int[] puzzel;
+    private int puzzleRow;
+    private int puzzleCells;
+    private int moves;
+    private int seconds;
+    private int emptyIndex;
+
+    private JButton newGameButton;
+    private JButton losningButton;
+    private JButton aterstallButton;
+    private JButton installningButton;
+
+    private JLabel labelOne;
+    private JLabel labelTwo;
+    private JLabel movesLabel;
+    private JLabel secondsLabel;
+
+    PuzzleDialog ()
+    {
+        puzzleRow = 4;
+        puzzleCells = 4;
+        puzzel = new int[puzzleRow * puzzleCells];
+        moves = 0;
+        seconds = 0;
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(1, 3));
+        this.add(mainPanel);
+
+        setTitle("Game Dialog");
+        setSize(1600, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
