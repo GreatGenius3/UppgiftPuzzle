4c4e8564a50c4ace61545fc9b83fa9da43614d0c
Martin Danmo <martindanmo81@gmail.com>
2025-11-06 10:25:43 +0100

    Uppdaterat funktioner

--- src/GameInt.java
+++ src/GameInt.java
@@ -1,6 +1,7 @@
 // Game interface
 public interface GameInt
 {
+    // Några enumerationer
     enum GameStatus { PAUSED, PLAYING, SOLVED }
     enum BrickDirection { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT }
     void newGame(int row, int cells);
